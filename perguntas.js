f40c546962e834a21ec7d10d7f528c51ca19854f
GabrielFagundesZaleski <146601510+GabrielFagundesZaleski@users.noreply.github.com>
2024-10-07 11:50:26 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+
+CriaCartao('Geografia', 'Qual é o maior continente do mundo?', 'Ásia')
+
+CriaCartao('Geografia', 'Qual é o oceano mais extenso?', 'Oceano Pacífico')
+
+CriaCartao('Geografia', 'Qual é o país com a maior população?', 'China')
+
+CriaCartao('Geografia', 'Qual é o rio mais longo do mundo?', 'Rio Nilo')
+
+CriaCartao('Geografia', 'Qual é o deserto mais quente do mundo?', 'Deserto do Saara',)
+
+
+
+CriaCartao('Matemática', 'Qual é a fórmula para calcular a área de um círculo?', 'A = πr²')
+
+CriaCartao('Matemática', 'Qual é o valor de π (pi)?', 'Aproximadamente 3,14')
+
+CriaCartao('Matemática', 'O que é uma equação de segundo grau?', 'Uma equação da forma ax² + bx + c = 0')
+
+CriaCartao('Matemática', 'Qual é a raiz quadrada de 16?', '4')
+
+CriaCartao('Matemática', 'O que são números primos?', 'Números naturais maiores que 1 e divisíveis apenas por 1 e por ele mesmo.')
+
+
+
+CriaCartao('Português', 'Qual é a diferença entre substantivo e adjetivo?', 'Substantivo nomeia seres, objetos ou ideias. Adjetivo caracteriza o substantivo.')
+
+CriaCartao('Português', 'O que é um verbo?', 'Palavra que expressa ação, estado ou fenômeno.')
+
+CriaCartao('Português', 'Quais são as três pessoas do discurso?', 'Primeira, segunda e terceira.')
+
+CriaCartao('Português', 'O que é um sinônimo?', 'Palavra com significado semelhante.')
+
+CriaCartao('Português', 'O que é um antônimo?', 'Palavra com significado oposto.')
+
+
+
+CriaCartao('Química', 'Qual é o símbolo químico da água?', 'H₂O')
+
+CriaCartao('Química', 'Qual é a diferença entre um átomo e uma molécula?', 'Átomo é a menor partícula de um elemento. Molécula é um grupo de átomos unidos.')
+
+CriaCartao('Química', 'O que é uma reação química?', 'Processo no qual uma ou mais substâncias se transformam em outras.')
+
+CriaCartao('Química', 'Qual é a lei da conservação das massas?', 'Em uma reação química, a massa dos reagentes é igual à massa dos produtos.')
+
+CriaCartao('Química', 'Qual é a diferença entre um elemento e um composto?', 'Elemento é formado por um único tipo de átomo. Composto é formado por dois ou mais tipos de átomos.')
+
