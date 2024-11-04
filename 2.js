4d6e02d15a3370d04aeef546a86ec760ece9f9e4
GabrielFagundesZaleski <146601510+GabrielFagundesZaleski@users.noreply.github.com>
2024-11-04 10:44:42 -0300

    v

--- 2.js
+++ 2.js
@@ -1,18 +1,29 @@
 function CriaCartao (categoria, pergunta, resposta){
-    let contaimer = document.getElementById('contaimer');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao';
-    cartao.innerHTML=`<div class="conteudo-cartao">
-      <h3>${categoria}</h3>
-      <div class="pergunta-cartao">
-        <p>${pergunta}</p>
-        </div>
-        <div class=${"resposta-cartao"}>
-          <p>${resposta}</p>
-      </div>
-    </div> `
-    contaimer.appendChild(cartao)
-    }
-    
+  let contaimer = document.getElementById('contaimer');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+  cartao.innerHTML =`<div class="conteudo-cartao" onclick="ViraCartao(this)">
+  <h3>${categoria}</h3>
+  <div class="pergunta-cartao">
+      <p>${pergunta}</p>
+  </div>
+  <div class=${"resposta-cartao"}>
+   <p>${resposta}</p>
+  </div>
+</div>
+    `
+
+    contaimer.appendChild(cartao);
+  }
+
+function ViraCartao (card){
+ const resposta = card.querySelector('.resposta-cartao');
 
 
+if (resposta.style.display === 'none'){
+ resposta.style.display = 'block';
+}
+else{
+ resposta.style.display = 'none';
+}
+}
